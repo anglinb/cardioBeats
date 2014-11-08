d25429a163a352e0c86f78aa6250e20e8a37f4b7
Brian <laxnski@gmail.com>
2014-11-08 11:25:44 -0800

    Adds basics

--- index.js
+++ index.js
@@ -11,3 +11,38 @@ server.listen(port, function () {
 
 // Routing
 app.use(express.static(__dirname + '/public'));
+
+
+var songs = [
+  {
+    "bpm":152,
+    "name":"Headlines by Drake",
+    "source":"songs/headlines.mp3",
+    "cover":"http://www.aceshowbiz.com/images/news/drake-broody-official-take-care-cover-art.jpg",
+  },
+  // {
+  //   "bpm":
+  // }
+];
+
+            // songMeta.bpm = data.songBPM;
+            // songMeta.name = data.songName;
+            // songMeta.source = data.songsource;
+            // songMeta.Cover = data.songCover;
+
+
+io.on('connection', function (socket) {
+  var addedUser = false;
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('command input', function (data) {
+    socket.broadcast.emit('bpm update', {
+      bpm: data.bpm
+    });
+    // we tell the client to execute 'new message'
+    // data.bpm = 
+
+
+  });
+
+});
